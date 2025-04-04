c5fc3f30f9cc7a151c1261dc0960cd6a3b991d84
Rosari28 <72675997+Rosari28@users.noreply.github.com>
2025-04-04 16:08:57 -0500

    Update api.py

--- api.py
+++ api.py
@@ -8,9 +8,14 @@ model = joblib.load("modelo_admision.pkl")
 # Inicializar FastAPI
 app = FastAPI()
 
-# Definir la ruta de predicción
+# Ruta base para verificar que la API funciona
+@app.get("/")
+def home():
+    return {"mensaje": "La API está activa. Usa /predecir para hacer una predicción."}
+
+# Ruta de predicción
 @app.post("/predecir")
 def predecir(data: dict):
     df = pd.DataFrame([data])
-    probabilidad = model.predict_proba(df)[:, 1][0]  # Obtener probabilidad
+    probabilidad = model.predict_proba(df)[:, 1][0]
     return {"probabilidad_admision": probabilidad}
